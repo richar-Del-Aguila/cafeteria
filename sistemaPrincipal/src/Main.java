8dad7484bb9a64ab2face1baed6b3e0d477c7698
Richar Del Aguila <richarredaa@gmail.com>
2026-03-12 23:32:06 -0600

    Version final del programa

--- sistemaPrincipal/src/Main.java
+++ sistemaPrincipal/src/Main.java
@@ -4,62 +4,85 @@ import java.util.List;
 
 public class Main{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String nombre;
-        String producto = "";
-        double precio = 0;
-        int cantidad = 0;
-        boolean menu = true;
 
-        System.out.println("Bienvenido");
-        System.out.println("Ingresa tu nombre: ");
+
+        //Variables que se le pediran al usuario
+        String nombre = "";
+        String pedido;
+        int cantidad = 0;
+
+        //Variables que usara el sistema
+        Scanner sc = new Scanner(System.in);
+        boolean menu = true;
+        String op = "S";
+        boolean op2 = true;
+
+        System.out.println("Bienvenido! \n Para continuar, ingresa tu  nombre: ");
         nombre = sc.nextLine();
 
-        while(menu){
-            System.out.println("Que deseas ordenar");
-            System.out.println("-------------------------------- \n" +
-                    "1. Cafe negro.....................Q10.00 \n" +
-                    "2. Cafe latte.....................Q15.00 \n" +
-                    "3. Cafe capuccino.................Q20.00 \n" +
-                    "4. Cafe helado....................Q22.00"
-            );
-            producto = sc.nextLine();
-            switch (producto) {
+        //Se declara el objeto
+        Pedidos cliente1 = new Pedidos();
+
+        while (menu){
+            System.out.println("Que deseas ordenar:");
+            System.out.println("1. Cafe negro..................Q.10 \n" +
+                               "2. Cafe latte..................Q.15 \n" +
+                               "3. Cafe con leche..............Q.17 \n" +
+                               "4. Cafe helado.................Q.20 \n" +
+                               "5. Cafe con pan................Q.25 \n" +
+                               "6. salir...........................");
+            pedido = sc.nextLine();
+
+
+            if(Integer.parseInt(pedido) >= 1 && Integer.parseInt(pedido) <= 5){
+                System.out.println("Ingrese la cantidad: ");
+                cantidad = Integer.parseInt(sc.nextLine());
+            } else if(Integer.parseInt(pedido) == 6){
+                System.out.println("Hasta luego! Por gusto te atendi");
+                menu = false;
+            } else {
+                System.out.println("Lo que usted pidio no esta en el menú");
+            }
+
+            System.out.println("Orden procesada....................");
+
+            //Se registrara el pedido en las listas
+            switch (pedido){
                 case "1":
-                    producto = "Cafe negro";
-                    precio = 10;
-                    menu = false;
+                    cliente1.gestionarPedido(cantidad, "Cafe negro");
                     break;
-
                 case "2":
-                    producto = "Cafe latte";
-                    precio = 15;
-                    menu = false;
+                    cliente1.gestionarPedido(cantidad, "Cafe latte");
                     break;
-
                 case "3":
-                    producto = "Cafe capuccino";
-                    precio = 20;
-                    menu = false;
+                    cliente1.gestionarPedido(cantidad, "Cafe con leche");
                     break;
-
                 case "4":
-                    producto = "Cafe helado";
-                    precio = 22;
-                    menu = false;
+                    cliente1.gestionarPedido(cantidad, "Cafe helado");
                     break;
-                default:
-                    System.out.println("Producto no encontrado");
-                    menu = true;
+                case "5":
+                    cliente1.gestionarPedido(cantidad, "Cafe con pan");
+                    break;
+            }
+
+            while (op2){
+                System.out.println("Desea ordenar algo más? S/N");
+                op = sc.nextLine();
+                op = op.toUpperCase();
+
+                if (op.equals("S") || op.equals("N")){
+                    op2 = false;
+                }
+            }
+
+            if (op.equals("S")){
+                menu = true;
+                op2 = true;
+            } else {
+                menu = false;
             }
         }
+        cliente1.Factura();
 
-        System.out.println("Ingrese la cantidad del producto: ");
-        cantidad = Integer.parseInt(sc.nextLine());
-
-        Pedidos pedidoCliente = new Pedidos(nombre, producto, cantidad, precio);
-        pedidoCliente.Descuentos();
-
-        pedidoCliente.Factura();
     }
 }
--- sistemaPrincipal/src/Pedidos.java
+++ sistemaPrincipal/src/Pedidos.java
@@ -1,46 +1,74 @@
-public class Pedidos {
-    String cliente;
-    String pedido;
-    int cantidad;
-    double precio;
-    double compraTotal;
-    double precioPagar;
-    double descuento;
+import java.util.ArrayList;
 
-    public Pedidos(String cliente, String pedido, int cantidad, double precio){
-        this.cliente = cliente;
-        this.pedido = pedido;
-        this.cantidad = cantidad;
-        this.precio = precio;
-        this.compraTotal = cantidad * precio;
+public class Pedidos {
+    //Declaración de listas para varias ordenes en el mismo pedido
+    ArrayList<Integer> cantidadesProducto = new ArrayList<>();
+    ArrayList<String> pedidos = new ArrayList<>();
+    ArrayList<Double> precios = new ArrayList<>();
+
+    //Variables para metodos
+    double totalPagar;
+
+    public void gestionarPedido(int cantidad, String producto){
+        if (pedidos.contains(producto)){
+            int indice = pedidos.indexOf(producto);
+            cantidadesProducto.set(indice, cantidadesProducto.get(indice) + cantidad);
+        } else {
+            pedidos.add(producto);
+            cantidadesProducto.add(cantidad);
+        }
     }
 
-    //Aplicar_descuentos
-    public void Descuentos(){
-        if (compraTotal >= 100){
-            descuento = 0.15;
-            precioPagar = compraTotal * (1 - descuento);
-        } else if (compraTotal >= 80){
-            descuento = 0.10;
-            precioPagar = compraTotal * (1 - descuento);
-        } else if (compraTotal >= 50){
-            descuento = 0.05;
-            precioPagar = compraTotal * (1 - descuento);
+    public void datosFactura(){
+        precios.clear();
+        totalPagar = 0.0;
+
+        for (int i = 0; i < pedidos.size(); i++){
+            switch (pedidos.get(i)){
+                case "Cafe negro":
+                    precios.add(cantidadesProducto.get(i) * 10.0);
+                    break;
+                case "Cafe latte":
+                    precios.add(cantidadesProducto.get(i) * 15.0);
+                    break;
+                case "Cafe con leche":
+                    precios.add(cantidadesProducto.get(i) * 17.0);
+                    break;
+                case "Cafe helado":
+                    precios.add(cantidadesProducto.get(i) * 20.0);
+                    break;
+                case "Cafe con pan":
+                    precios.add(cantidadesProducto.get(i) * 25.0);
+                    break;
+            }
+        }
+
+        for (int j = 0; j < pedidos.size(); j++){
+            totalPagar = totalPagar + precios.get(j);
+        }
+    }
+
+    public int verificarCantidades(String product){
+        int indice = pedidos.indexOf(product);
+        if (indice == -1){
+            return 0;
         } else {
-            precioPagar = compraTotal;
+            return cantidadesProducto.get(indice);
         }
     }
 
     public void Factura(){
-        System.out.println("-------------------------------------------------");
-        System.out.println("| Factura de cafeteria Cafe con aroma a mujer |");
-        System.out.println("-------------------------------------------------");
-        System.out.println("|Producto ordenado..............." + pedido);
-        System.out.println("|Cantidad ordenada..............." + cantidad);
-        System.out.println("|Precio unitario.................Q." + precio);
-        System.out.println("-------------------------------------------------");
-        System.out.println("|Total a neto....................Q." + compraTotal);
-        System.out.println("|Descuento aplicado.............." + (descuento * 100) + "%");
-        System.out.println("|Total a pagar...................Q." + precioPagar);
+        datosFactura();
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Cafeteria `Cafe con aroma a mujer` \n" +
+                           "--------------------Detalles de la orden--------------------");
+        System.out.println("Cafe negro..................Q.10 cantidad: " + verificarCantidades("Cafe negro"));
+        System.out.println("Cafe latte..................Q.15 cantidad: " + verificarCantidades("Cafe latte"));
+        System.out.println("Cafe con leche..............Q.17 cantidad: " + verificarCantidades("Cafe con leche"));
+        System.out.println("Cafe helado.................Q.20 cantidad: " + verificarCantidades("Cafe helado"));
+        System.out.println("Cafe con pan................Q.25 cantidad: " + verificarCantidades("Cafe con pan"));
+
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Total a pagar..........................................Q." + totalPagar);
     }
 }
